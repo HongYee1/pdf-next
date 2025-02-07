b9cdf0c82ff4a32a189491d215d51f01a6362092
hongye <1514570018@qq.com>
2025-02-07 16:18:06 +0800

    header 布局
    Signed-off-by: hongye <1514570018@qq.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header className='flex justify-between items-center p-[10px] h-[50px]'>
-            <a href="/">Logo</a>
-            <nav className='flex gap-4'>
-                <a href="/about" className='hover:underline'>Pricing</a>
-                <a href="/about" className='hover:underline'>Chorme extension</a>
-                <a href="/about" className='hover:underline'>Use cases</a>
-                <div>EN</div>
-                <a href="/about" className='hover:underline'>Get started →</a>
+            <a href="/" className='p-[10px]'>Logo</a>
+            <nav className='flex text-[15px] font-medium'>
+                <a href="/pricing" className='hover:underline p-[10px]'>Pricing</a>
+                <a href="/chrome-extension" className='hover:underline p-[10px]'>Chorme extension</a>
+                <a href="/use-cases" className='hover:underline p-[10px]'>Use cases</a>
+                <div className='p-[10px]'>EN</div>
+                <a href="/sign-in" className='hover:underline p-[10px]'>Get started →</a>
             </nav>
         </header>
     );
